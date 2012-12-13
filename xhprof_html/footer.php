323235b5c6305cd33bb5672d02a49fc88ec8816e
Salimane Adjao Moustapha <me@salimane.com>
2012-12-13 18:01:06 +0800

    take care long urls which are > 256 and can't be saved as filename

--- xhprof_html/footer.php
+++ xhprof_html/footer.php
@@ -3,7 +3,9 @@ if (extension_loaded('xhprof') && !is_xhprof_url()) {
   $profiler_namespace = 'myapp'; // namespace for your application
   $xhprof_data = xhprof_disable();
   $xhprof_runs = new XHProfRuns_Default();
-  $url_id = (isset($_SERVER["SERVER_NAME"]) ? strtr($_SERVER["SERVER_NAME"].(isset($_SERVER["REQUEST_URI"]) ? $_SERVER["REQUEST_URI"]: ""), " /.&=:?%", "________") : "")."_".uniqid();
+  $url_id = (isset($_SERVER["SERVER_NAME"]) ? strtr($_SERVER["SERVER_NAME"].(isset($_SERVER["REQUEST_URI"]) ? $_SERVER["REQUEST_URI"]: ""), " /.&=:?%", "________") : "");
+  $url_id = substr($url_id, 0, 200);
+  $url_id .= "_".uniqid();
   $run_id = $xhprof_runs->save_run($xhprof_data, $profiler_namespace, $url_id);
 
   // url to the XHProf UI libraries (change the host name and path)
